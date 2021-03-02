613b3367fd74d8add8eb8678ef1052fa80213d21
thales <jiangtai20@mails.ucas.ac.cn>
2021-03-02 16:54:56 +0800

    更改识别显示方式

--- predict.py
+++ predict.py
@@ -4,3 +4,14 @@ recognizer = Recognizer('./recognition.json')
 recognizer.load()
 result = recognizer.predict('./predictions/')
 print(result)
+
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+fig, axes = plt.subplots(1, len(result), figsize=(9, 5))
+for idx, obj in enumerate(result):
+  img = plt.imread(obj['img'])
+  axes[idx].imshow(img)
+  axes[idx].set_title(obj['label'])
+plt.show()
